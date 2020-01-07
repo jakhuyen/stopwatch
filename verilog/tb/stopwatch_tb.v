68f2e3d184fd7e250727aca0fd88d63fee14d2c0
Jak Huyen <jakhuyen@yahoo.com>
2020-01-06 18:40:39 -0700

    Updated testbench to be stopwatch_tb instead of counter_tb.

--- verilog/tb/stopwatch_tb.v
+++ verilog/tb/stopwatch_tb.v
@@ -1,20 +1,12 @@
-`include "../src/counter.v"
+// Default clock for Nexys 4 DDR is 100 MHz.
 
-module counter_tb;
+module stopwatch_tb;
     reg r_clk = 1'b0;
     reg r_rst = 1'b0;
     wire[1:0] r_cnt_out;
     wire r_cnt_done;
 
     parameter integer tb_cnt = 6;
-
-    counter #(.MAX_CNT(tb_cnt), .LOOP(1)) UUT
-    (
-        .i_clk(r_clk),
-        .i_rst(r_rst),
-        .o_cnt_done(r_cnt_done),
-        .o_cnt_val(r_cnt_out)
-    );
 
     always #1 r_clk <= !r_clk;
 
